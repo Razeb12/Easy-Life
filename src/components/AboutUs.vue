b7428c32b6e4304dfe6502e3fd3877776d740599
Razeb Enyi <enyirazeb@gmail.com>
2021-01-18 22:51:47 +0100

    Vue

---
+++ src/components/AboutUs.vue
@@ -0,0 +1,200 @@
+<template>
+    <div>
+ <section id="aboutUs">
+
+    <div class="inner_wrapper aboutUs-container fadeInLeft animated wow">
+      <div class="container">
+        <h2>About Us</h2>
+        <h6>Lorem ipsum dolor sit amet, consectetur Morbi sagittis, sem quisci ipsum</h6>
+        <div class="inner_section">
+
+          <div class="row">
+            <div class="col-lg-12 about-us">
+              <div class="row">
+             
+                <div class="col-md-6">
+                  <h3>We Make your Homes more beutiful</h3>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur Morbi sagittis, sem quisci ipsum gravida tortor orci ipsum
+                    gravida tortor. Lorem ipsum dolor sit amet, consectetur.
+                  </p>
+
+                  <ul class="about-us-list">
+                    <li class="points">Consectetur Morbi sagittis, sem quisci ipsum gravida tortor</li>
+                    <li class="points">Morbi sagittis, sem quis lacinia faucibus, orci ipsum gravida </li>
+                    <li class="points">Lorem ipsum dolor sit amet, consectetur Morbi sagittis, sem quisci gravida </li>
+                    <li class="points">Sagittis, sem quis lacinia faucibus, orci ipsum gravida </li>
+                    <li class="points">Ipsum dolor sit amet, consectetur Morbi sagittis, sem quisci</li>
+                  </ul>
+                </div>
+
+                <div class="col-md-6">
+                 <!-- <img src="img/dinning-room-695074.jpg" alt="" class="container-fluid"> -->
+                 <div class="wrapper">
+                    <div class="pic-ctn">
+                      <img src="../assets/dinning-room-695074.jpg" alt="" class="pic">
+                      <img src="../assets/1.jpg" alt="" class="pic">
+                      <img src="../assets/2.jpg" alt="" class="pic">
+                      <img src="../assets/3.jpg" alt="" class="pic">
+                      <img src="../assets/furniture-998265_1920.jpg" alt="" class="pic">
+                    </div>
+                 </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+        </div>
+      </div>
+    </div>
+  </section>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "AboutUs",
+};
+</script>
+
+
+<style>
+.aboutUs-container {
+  background-repeat: no-repeat;
+  background-position: right;
+  background-size: auto 100%;
+  background-position-y: 70px;
+}
+
+#about-us .read-more {
+  border: 1px solid #efb21d;
+  color: #1a1a1a;
+}
+
+#about-us .read-more:hover {
+  background-color: #e74c3c;
+  border: 1px solid #e74c3c;
+  color: #fff;
+}
+
+.about-us-post-container {
+  background-repeat: no-repeat;
+  background-position: right;
+  background-size: auto 100%;
+}
+
+.about-us {
+  margin-top: 50px;
+  margin-bottom: 50px;
+}
+
+.about-us h3 {
+  font-size: 28px;
+}
+
+.points {
+  margin-bottom: 10px;
+  font-size: 14px;
+  padding-left: 0px;
+}
+
+.about-us p {
+  /*font-size: 0.9375em;*/
+  line-height: 1.625em;
+  padding: 15px 0;
+}
+
+.about-us .about-us-list {
+  list-style: none;
+  margin-bottom: 20px;
+  padding-left: 0px;
+}
+
+.about-us .points {
+  position: relative;
+  list-style-type: none;
+  padding: 5px 0 5px 30px;
+}
+
+.about-us .points:before {
+  content: "\f058";
+  position: absolute;
+  font-family: FontAwesome;
+  font-size: 1.125em;
+  color: #efb21d;
+  left: 0px;
+  top: 3px;
+}
+
+.about-us-link a:first-child {
+  margin-left: 0;
+}
+#aboutUs {
+  padding: 60px 0;
+}
+.pic-ctn {
+    width: 100vw;
+    height: 200px;
+}
+
+@keyframes display {
+    0% {
+        transform: translateX(200px);
+        opacity: 0;
+    }
+
+    10% {
+        transform: translateX(0);
+        opacity: 1;
+    }
+
+    20% {
+        transform: translateX(0);
+        opacity: 1;
+    }
+
+    30% {
+        transform: translateX(-200px);
+        opacity: 0;
+    }
+
+    100% {
+        transform: translateX(-200px);
+        opacity: 0;
+    }
+}
+.pic-ctn {
+    position: relative;
+    width: 100vw;
+    height: 300px;
+    /* margin-top: 15vh; */
+}
+
+.pic-ctn>img {
+    position: absolute;
+    top: 0;
+    left: calc(50% - 100px);
+    opacity: 0;
+    animation: display 20s infinite;
+}
+
+img:nth-child(2) {
+    animation-delay: 4s;
+}
+
+img:nth-child(3) {
+    animation-delay: 8s;
+}
+
+img:nth-child(4) {
+    animation-delay: 12s;
+}
+
+img:nth-child(5) {
+    animation-delay: 16s;
+}
+img {
+    max-width: 100%;
+}
+
+</style>
---
+++ src/components/MainSection.vue
@@ -0,0 +1,135 @@
+<template>
+  <div class="hello">
+     <section id="service">
+    <div class="container">
+      <h2>Explore Our Services</h2>
+      <div class="service_wrapper">
+        <div class="row">
+          <div class="col-lg-3">
+            <div class="service_icon delay-03s animated wow  zoomIn">
+              <span><img src="../assets/1.jpg" alt="" /> </span> </div>
+            <div class="service_block">
+
+              <h3 class="animated fadeInUp wow">Home Cleaning</h3>
+              <p class="animated fadeInDown wow">Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. </p>
+            </div>
+          </div>
+          <div class="col-lg-3">
+            <div class="service_icon icon2  delay-03s animated wow zoomIn">
+              <span><img src="../assets/lawnmower-384589_1920.jpg" alt="" class=" img-fluid"/> </span> 
+            </div>
+            <div class="service_block">
+              <h3 class="animated fadeInUp wow">Grass/Cutting Lawn</h3>
+              <p class="animated fadeInDown wow">Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. </p>
+            </div>
+          </div>
+          <div class="col-lg-3">
+            <div class="service_icon icon3  delay-03s animated wow zoomIn">
+              <span><img src="../assets/laundry-413688_1920.jpg" alt="" /> </span> </div>
+            <div class="service_block">
+
+              <h3 class="animated fadeInUp wow">Laundering and dry cleaning</h3>
+              <p class="animated fadeInDown wow">Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. </p>
+            </div>
+          </div>
+          <div class="col-lg-3">
+            <div class="service_icon icon3  delay-03s animated wow zoomIn">
+              <span><img src="../assets/hand-disinfection-4954840_1920.jpg" alt="" /> </span> </div>
+            <div class="service_block">
+
+              <h3 class="animated fadeInUp wow">Disinfection</h3>
+              <p class="animated fadeInDown wow">Lorem Ipsum is simply dummy text of the printing and typesetting
+                industry. </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    
+  </section>
+  <!--Service-->
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MainSection',
+  
+}
+
+
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+
+#service {
+    padding: 60px 0px;
+    background:#374045;
+}
+
+#service h2 {
+    color: #fff;
+
+
+}
+
+.service_wrapper {
+    padding: 40px 0 40px;
+}
+
+
+.service_block {
+    text-align: center;
+    padding-left: 15px;
+    padding-right: 15px;
+    position: relative;
+}
+
+.service_icon {
+    width: 80%;
+    height: 144px;
+    margin: 0px auto 25px;
+}
+
+
+
+.service_icon span {
+    background: transparent;
+    display: block;
+    height: 100%;
+    width: 100%;
+    border: 1px solid #fff;
+    overflow: hidden;
+}
+
+.service_icon i {
+    color: #fff;
+    font-size: 40px;
+    margin-top: 0;
+    position: relative;
+    top: 20px;
+    z-index: 20;
+    display: block;
+    text-align: center;
+}
+
+.service_block h3 {
+    font-family: 'Raleway', sans-serif;
+    font-weight: 600;
+    font-size: 25px;
+    color: #fff;
+    margin: 35px 0 15px;
+}
+
+.service_block p {
+    font-size: 16px;
+    color: #fff;
+    margin: 0px;
+}
+</style>
